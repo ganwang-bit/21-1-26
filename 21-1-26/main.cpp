6cc8ba5112c9426fb95d32987303c9b232bb2305
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-01-26 18:15:46 +0800

    21-1-26

---
+++ 21-1-26/main.cpp
@@ -0,0 +1,75 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//    return 0;
+//}
+//#define FILENAME "21-1-26.txt"
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//int main()
+//{
+//    fstream f;
+//    f.open(FILENAME,ios::out|ios::binary);
+//    if(f.is_open())
+//        cout<<"打开成功"<<endl;
+//    else
+//        cout<<"文件不存在"<<endl;
+//
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//#include<iomanip>
+//int main()
+//{
+//    ifstream f;
+//    f.open(FILENAME,ios::in|ios::app);
+//    f.close();
+////    cout<<setiosflags(ios::left)<<setw(10)<<10<<endl;
+//    return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//    fprintf(stdout,"%d",1);
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//#include<string>
+//int main()
+//{
+////    ofstream wf;
+////    wf.open("21-1-26.txt",ios::out);
+////    wf<<"张三"<<" "<<18<<endl;
+//    int age;
+//    string name;
+////    wf.close();
+//    ifstream rf;
+//    rf.open("21-1-26.txt",ios::in|ios::app);
+//    if(!rf.is_open())
+//    {
+//        cout<<"文件打开失败"<<endl;
+//        return 0;
+//    }
+//    rf>>name>>age;
+//    cout<<name<<" "<<age<<endl;
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//int main()
+//{
+//    ifstream f;
+//    f.read()
+//    return 0;
+//}
